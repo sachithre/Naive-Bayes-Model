78cce7de32d4a61a1b5742795395053863b97c9f
wsachithregit <wsachithre@gmail.com>
2019-07-01 00:26:58 +0530

    First Commit

---
+++ LensePredict/src/LensePredict.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class LensePredict {
+
+	public static void main(String[] args) throws Exception {
+		FileReader reader = new FileReader("C:\\Users\\Sachithre\\eclipse-workspace\\LensePredict\\src\\lense.arff"); // read the dataset file
+		Instances instance_train = new Instances(reader); //create an instance
+		BufferedReader fi = new BufferedReader(reader); //read the file and pass data to buffer
+		fi.close(); // file close
+		
+		instance_train.setClassIndex(4); //we are gonna set 5th attribute as class
+		NaiveBayes n = new NaiveBayes();
+		
+		n.buildClassifier(instance_train); //load data to weka built in function
+		System.out.println("Naive Bayes model");
+		System.out.println(n.toString());
+		
+		Instance inst = new DenseInstance(4);
+		inst.setDataset(instance_train);
+		
+		inst.setValue(0, "pre-presbyopic"); 
+		inst.setValue(1, "hypermetrope"); 
+		inst.setValue(2, "no"); 
+		inst.setValue(3, "normal"); 
+		
+		System.out.println("The instance: " + inst); 
+		System.out.println(n.classifyInstance(inst));
+
+	}
+
+}
---
+++ LensePredict/src/LensePredictTree.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import weka.classifiers.trees.J48;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class LensePredictTree {
+
+	public static void main(String[] args) throws Exception {
+		FileReader reader = new FileReader("C:/Users/cst15029/Desktop/Lenses.arff"); // read the dataset file
+		Instances instance_train = new Instances(reader); //create an instance
+		instance_train.setClassIndex(4);
+
+		J48 j48 = new J48();
+		
+		j48.buildClassifier(instance_train);
+		System.out.println("Decision Tree model");
+		System.out.println(j48.toString());
+		
+		Instance inst = new DenseInstance(4);
+		inst.setDataset(instance_train);
+		
+		inst.setValue(0, "pre-presbyopic"); 
+		inst.setValue(1, "myope"); 
+		inst.setValue(2, "yes"); 
+		inst.setValue(3, "normal"); 
+		
+		System.out.println("The instance: " + inst); 
+		System.out.println(j48.classifyInstance(inst));		
+		
+	}
+
+}
